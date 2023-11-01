60d2b4d2951d199090adf001171f9609c9dde794
aniket-git-hub <singhdharmvir81@gmail.com>
2023-11-01 17:43:42 +0530

    initial template setup

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+	return (
+		<main>
+			<h1 className="text-2xl font-bold text-red-600 ">About</h1>
+		</main>
+	)
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+	return (
+		<main>
+			<h1 className="text-2xl font-bold text-red-600 ">Contact</h1>
+		</main>
+	)
+}
